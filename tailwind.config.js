3e9fb73caa4d3affdc0474015cfa7ee033bcce4b
Tami1118 <chenziyi93326@gmail.com>
2024-03-12 14:19:51 +0800

    :lipstick: Add: color and font-size add

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,30 @@ export default {
     "./src/views/**/*.{vue, js, ts, jsx, tsx, scss}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#13ab67',
+      },
+      fontSize: {
+        '3':  '12px',
+        '3h': '14px',
+        '4':  '16px',
+        '4h': '18px',
+        '5':  '20px',
+        '5h': '22px',
+        '6':  '24px',
+        '6h': '26px',
+        '7':  '28px',
+        '8':  '32px',
+        '9':  '36px',
+        '10': '40px',
+        '12': '48px',
+        '13': '52px',
+        '16': '64px',
+        '18': '72px',
+        '24': '96px',
+      },
+    },
   },
   plugins: [],
 }
