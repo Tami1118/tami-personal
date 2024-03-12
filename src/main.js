32e6c1dbecb9ddcb30f50ab18cf2160d292799ee
Tami1118 <chenziyi93326@gmail.com>
2024-03-12 14:18:41 +0800

    :package: Add: fortawesome add

--- src/main.js
+++ src/main.js
@@ -6,9 +6,18 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// FontAwesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(fas, fab, far);
+
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+   .use(router)
+   .component("font-awesome-icon", FontAwesomeIcon)
 
 app.mount('#app')
